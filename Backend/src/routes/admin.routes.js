fb80d0363dcc91b71f38a956dc35ed546e512056
Firdaun <ketuakls776@gmail.com>
2026-01-29 08:16:46 +0700

    menambahkan frontend2

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -3,8 +3,6 @@ import { memberController } from "../controller/member.controller.js"
 
 const adminRouter = new express.Router()
 
-// Admin boleh NAMBAH & HAPUS
 adminRouter.post('/api/admin/members', memberController.add)
-// adminRouter.delete('/api/admin/members/:id', memberController.remove)
 
 export { adminRouter }
